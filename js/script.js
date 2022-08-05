3b6379787c8801e0f3a5c94e935059ff741c3037
Heric Olier <hericolier.code@gmail.com>
2022-08-05 15:56:56 -0500

    add Search Bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 let headerArea = document.querySelector(".header__area");
+let iconSearch = document.querySelector(".fa-magnifying-glass");
+let searchBar = document.querySelector(".search__bar");
+let searchBarInput = document.querySelector("#search");
+let searchBarClose = document.querySelector(".search__close-btn");
 
 //*!--------------- scroll up & Header Area ---------------*//
 
@@ -10,4 +14,22 @@ window.addEventListener("scroll", () => {
   } else {
     headerArea.classList.remove("active");
   }
+});
+
+//*!--------------- Search bar ---------------*//
+
+// al click del icono, aparece el input
+
+iconSearch.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+  searchBarInput.focus();
+  searchBarInput.value = "";
+});
+
+// al click del boton de cerrar, se cierra el input
+
+searchBarClose.addEventListener("click", () => {
+  searchBar.classList.remove("active");
+  searchBarInput.value = "";
+
 });
