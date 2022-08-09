a47f6b984a35b87744279bf20ef12cb97e049196
Heric Olier <hericolier.code@gmail.com>
2022-08-09 12:16:45 -0500

    fix Products Fetch Petititon

---
+++ controllers/console-products.js
@@ -0,0 +1,60 @@
+
+
+const consolesProducts = document.querySelector(".container__carousel-products-template-consoles"); //template de los productos de consolas
+const cardTemplate = document.querySelector("#card__template").content;
+
+
+async function getTypeProducts(typeProduct) {
+    try {
+        const response = await fetch(
+            `https://alura-geek-fake-appi-server.herokuapp.com/products?category=${typeProduct}`
+        );
+        const products = await response.json();
+        return products;
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+
+async function showConsoleProducts() {
+    try {
+        const products = await getTypeProducts("Consoles");
+
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement("div");
+        div.classList.add("consoles-products__carousel-content");
+
+        products.forEach(({ name, price, imageUrl }) => {
+            const card = cardTemplate.cloneNode(true);
+            card.querySelector(".products__template-subtitle").textContent = name;
+            card.querySelector(".products__template-price").textContent = price;
+            card.querySelector("img").src = imageUrl;
+            fragment.appendChild(card);
+        });
+
+        div.appendChild(fragment);
+        consolesProducts.appendChild(div);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        tns({
+            container: ".consoles-products__carousel-content",
+            items: 4,
+            slideBy: 1,
+            autoplay: true,
+            controls: false,
+            nav: false,
+            autoplayButtonOutput: false,
+            autoplayTimeout: 5000,
+            autoplayButton: false,
+            loop: true,
+            mouseDrag: true,
+        });
+    }
+}
+  
+showConsoleProducts();
+console.log(consolesProducts);
+
+ 
--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -1,12 +1,10 @@
 const newProducts = document.querySelector(".container__carousel-products-template"); //template de los productos nuevos
-const consolesProducts = document.querySelector(".container__carousel-products-template-consoles"); //template de los productos de consolas
-const joysticksProducts = document.querySelector(".container__carousel-products-template-joysticks"); // template de los productos de joysticos
 const cardTemplate = document.querySelector("#card__template").content;
 
 //creamos una funcion para traer los datos de todos los articulos desde la api json
 
 
-const getAllProducts = async () => {
+async function getAllProducts() {
   try {
     const response = await fetch(
       "https://alura-geek-fake-appi-server.herokuapp.com/products"
@@ -16,11 +14,11 @@ const getAllProducts = async () => {
   } catch (error) {
     console.log(error);
   }
-};
+}
 
 // creamos una funcion para traer los datos de un articulo especifico desde la api json
 
-const getTypeProducts = async (typeProduct) => {
+async function getTypeProducts(typeProduct) {
   try {
     const response = await fetch(
       `https://alura-geek-fake-appi-server.herokuapp.com/products?category=${typeProduct}`
@@ -30,29 +28,12 @@ const getTypeProducts = async (typeProduct) => {
   } catch (error) {
     console.log(error);
   }
-};
-
-// creamos una funcion para mostrar el carousel de los productos
-
-function tinyCarousel() {
-  tns({
-    container: ".new-products__carousel-content",
-    items: 4,
-    slideBy: 1,
-    autoplay: true,
-    controls: false,
-    nav: false,
-    autoplayButtonOutput: false,
-    autoplayTimeout: 5000,
-    autoplayButton: false,
-    loop: true,
-    mouseDrag: true,
-  });
 }
+
 
 // creamos una funcion para moostrar los productos nuevos
 
-const showNewProducts = async () => {
+async function showProducts() {
   try {
     const products = await getTypeProducts("NewProduct");
 
@@ -67,72 +48,26 @@ const showNewProducts = async () => {
       card.querySelector("img").src = imageUrl;
       fragment.appendChild(card);
     });
+
     div.appendChild(fragment);
     newProducts.appendChild(div);
   } catch (error) {
     console.log(error);
   } finally {
-    tinyCarousel();
-  }
-};
-
-
-// creamos una funcion para mostrar los productos de consolas
-
-const showConsolesProducts = async () => {
-  try {
-    const products = await getTypeProducts("Consoles");
-
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement("div");
-    div.classList.add("new-products__carousel-content");
-
-    products.forEach(({ name, price, imageUrl }) => {
-      const card = cardTemplate.cloneNode(true);
-      card.querySelector(".products__template-subtitle").textContent = name;
-      card.querySelector(".products__template-price").textContent = price;
-      card.querySelector("img").src = imageUrl;
-      fragment.appendChild(card);
+    tns({
+      container: ".new-products__carousel-content",
+      items: 4,
+      slideBy: 1,
+      autoplay: true,
+      controls: false,
+      nav: false,
+      autoplayButtonOutput: false,
+      autoplayTimeout: 5000,
+      autoplayButton: false,
+      loop: true,
+      mouseDrag: true,
     });
-    div.appendChild(fragment);
-    consolesProducts.appendChild(div);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    tinyCarousel();
   }
-};
+}
 
-
-
-
-// creamos una funcion para mostrar los productos de Joysticks
-
-const showJoysticsProducts = async () => {
-  try {
-    const products = await getTypeProducts("Joysticks");
-
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement("div");
-    div.classList.add("new-products__carousel-content");
-
-    products.forEach(({ name, price, imageUrl }) => {
-      const card = cardTemplate.cloneNode(true);
-      card.querySelector(".products__template-subtitle").textContent = name;
-      card.querySelector(".products__template-price").textContent = price;
-      card.querySelector("img").src = imageUrl;
-      fragment.appendChild(card);
-    });
-    div.appendChild(fragment);
-    joysticksProducts.appendChild(div);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    tinyCarousel();
-  }
-};
-
-
-showNewProducts();
-showConsolesProducts();
-showJoysticsProducts();
+showProducts();
