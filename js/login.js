63fcb10b992cb6a8516d09acfd5d83909f97b982
Heric Olier <hericolier.code@gmail.com>
2022-08-10 01:04:53 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,12 +2,12 @@ const loginArea = document.querySelector(".login__area");
 const canvasOverlayBlur = document.querySelector(".canvas__overlay-blur");
 const loginIcon = document.querySelector(".fa-user");
 const btnCloseLogin = document.querySelector(".fa-xmark");
-
-
+const loginAreaContent = document.querySelector(".login__area-content");
 
 const showLogin = () => {
   loginArea.classList.toggle("active");
   canvasOverlayBlur.classList.toggle("active");
+  loginAreaContent.classList.toggle("active");
 };
 
 loginIcon.addEventListener("click", () => {
@@ -15,5 +15,9 @@ loginIcon.addEventListener("click", () => {
 });
 
 btnCloseLogin.addEventListener("click", () => {
+  showLogin();
+});
+
+canvasOverlayBlur.addEventListener("click", () => {
   showLogin();
 });
