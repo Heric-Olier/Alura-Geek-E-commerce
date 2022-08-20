c7cab96dc963e4977ff702c6e4277e1e7c4eb6ff
Heric Olier <hericolier.code@gmail.com>
2022-08-20 14:59:23 -0500

    Add Responsive Tablet devices

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-
 let headerArea = document.querySelector(".header__area");
 let iconSearch = document.querySelector(".fa-magnifying-glass");
 let searchBar = document.querySelector(".search__bar");
 let searchBarInput = document.querySelector("#search");
 let searchBarClose = document.querySelector(".search__close-btn");
+const navBurguer = document.querySelector(".nav__burguer");
+const navMenu = document.querySelector(".nav__menu");
+const navMenuItems = document.querySelectorAll(".nav__menu ul li");
+const navBurguerLine1 = document.querySelector(".nav__burguer-line-1");
+const navBurguerLine2 = document.querySelector(".nav__burguer-line-2");
+const navBurguerLine3 = document.querySelector(".nav__burguer-line-3");
+
 
 //*!--------------- scroll up & Header Area ---------------*//
 
@@ -32,8 +38,29 @@ iconSearch.addEventListener("click", () => {
 searchBarClose.addEventListener("click", () => {
   searchBar.classList.remove("active");
   searchBarInput.value = "";
-
 });
 
-//*!--------------- Tiny Carousel ---------------*//
+//*!--------------- Burguer Menu ---------------*//
 
+
+const showNav = () => {
+  navMenu.classList.toggle("active");
+  navBurguerLine1.classList.toggle("active");
+  navBurguerLine2.classList.toggle("active");
+  navBurguerLine3.classList.toggle("active");
+  navBurguer.classList.toggle("active");
+};
+
+// El menu se muestra y se oculta con un click en el icono de hamburguesa
+navBurguer.addEventListener("click", () => {
+  showNav();
+  searchBar.classList.remove("active");
+});
+
+// El menú se cierra al hacer click en un elemento del mismo
+navMenuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    showNav();
+    searchBar.classList.remove("active");
+  });
+});
