45eb0d17985ccc7dfc172abc7a339c009bb1e58c
Heric Olier <hericolier.code@gmail.com>
2022-12-10 16:46:03 -0500

    Update countdown-hot-deal.js

--- js/countdown-hot-deal.js
+++ js/countdown-hot-deal.js
@@ -5,7 +5,7 @@ const countDownSeconds = document.querySelector('.countdown-number-seconds');
 
 const countDown = () => {
   const now = new Date();
-  const end = new Date('2022-12-10T00:00:00');
+  const end = new Date('2023-01-10T00:00:00');
   const timeLeft = end - now;
   const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
